2daa9b852473fbd9ed7925f11c7df77eb2effa8b
JH <https://github.com/jherman-mcc/MAD-105aboutme.git>
2021-05-31 23:31:37 -0500

    Initial commit

---
+++ main/src/main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var i=0
+    var iceCream = arrayOf<String>("vanilla   ","chocolate ","strawberry","Rocky Road","carmel    ","sugar free","yogurt   ")
+    while (i < iceCream.size) {
+        println("Element: ${iceCream[i]}")
+        i++
+    }
+}
